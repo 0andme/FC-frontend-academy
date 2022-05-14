4f4d3963b69c54b2b28c1a85bad2d9bfc6e468d4
0andme <yu00mi97@gmail.com>
2022-05-14 16:42:58 +0900

    feat : 뉴스 타이틀 클릭시 해당 뉴스의 타이틀 표시

--- hacker-news-client/app.js
+++ hacker-news-client/app.js
@@ -1,22 +1,40 @@
 const ajax = new XMLHttpRequest();
+// urls
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+
+const container = document.getElementById("root");
+const content = document.createElement("div");
+const ul = document.createElement("ul");
+container.appendChild(ul);
+container.appendChild(content);
+
 // 요청 초기화
 ajax.open("GET", NEWS_URL, false);
 //요청 보내기
 ajax.send();
 // 응답 데이터 가져오기
 const newsFeed = JSON.parse(ajax.response);
-// ul-li태그 추가하기
-document.getElementById("root").innerHTML = `<ul>
-  <li>${newsFeed[0].title}</li>
-  <li>${newsFeed[1].title}</li>
-  <li>${newsFeed[2].title}</li>
-</ul>`;
-// 반복코드
-const ul = document.createElement("ul");
-document.getElementById("root").appendChild(ul);
+
+// 뉴스 타이틀 클릭시 변경되는 hash 이벤트
+window.addEventListener("hashchange", function () {
+  // content 가져오기
+  const id = this.location.hash.substring(1);
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+  const newsContent = JSON.parse(ajax.response);
+  // 클릭된 타이틀의 타이틀 출력
+  const title = this.document.createElement("h1");
+  content.appendChild(title);
+  title.innerHTML = newsContent.title;
+});
+// 10개의 뉴스 타이틀
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement("a");
+  a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`;
+  a.href = `#${newsFeed[i].id}`;
+
   ul.appendChild(li);
+  li.appendChild(a);
 }
