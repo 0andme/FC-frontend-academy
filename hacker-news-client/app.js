46c8fc695842a6f0b185aace5499fb78da3b7967
0andme <yu00mi97@gmail.com>
2022-05-15 10:56:15 +0900

    feat : 타이틀 클릭 시 목록 대신 타이틀과 목록 이동 a 태그 표시

--- hacker-news-client/app.js
+++ hacker-news-client/app.js
@@ -4,10 +4,16 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 const container = document.getElementById("root");
-const content = document.createElement("div");
-const ul = document.createElement("ul");
-container.appendChild(ul);
-container.appendChild(content);
+
+// api 호출 함수
+function getData(url) {
+  // 요청 초기화
+  ajax.open("GET", url, false);
+  //요청 보내기
+  ajax.send();
+  // 응답 데이터 가져오기
+  return JSON.parse(ajax.response);
+}
 
 // api 뉴스 데이터 가져오기
 const newsFeed = getData(NEWS_URL);
@@ -18,32 +24,23 @@ window.addEventListener("hashchange", function () {
   const id = this.location.hash.substring(1);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
   // 클릭된 타이틀의 타이틀 출력
-  const title = this.document.createElement("h1");
-  content.appendChild(title);
-  title.innerHTML = newsContent.title;
+  container.innerHTML = `
+  <h1>${newsContent.title}</h1>
+  <div>
+    <a href='#'>목록으로</a>
+  </div>`;
 });
 
 // 10개의 뉴스 타이틀
+const newsList = [];
+newsList.push("<ul>");
 for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-  div.innerHTML = `
+  newsList.push(`
   <ul>
     <li>
       <a href='#${newsFeed[i].id}'>${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
     </li>
-  </ul>`;
-
-  // ul.appendChild(div.children[0]);
-  ul.appendChild(div.firstElementChild);
+  </ul>`);
 }
-
-// api 호출 함수
-
-function getData(url) {
-  // 요청 초기화
-  ajax.open("GET", url, false);
-  //요청 보내기
-  ajax.send();
-  // 응답 데이터 가져오기
-  return JSON.parse(ajax.response);
-}
+newsList.push("</ul>");
+container.innerHTML = newsList.join("");
